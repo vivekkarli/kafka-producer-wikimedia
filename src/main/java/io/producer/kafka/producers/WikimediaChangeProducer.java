509d35a551c24b381a7f8945dd3e7016368c481a
vivekkarli <vivekkarli7@gmail.com>
2024-06-29 19:45:33 +0530

    convert project to spring boot project

---
+++ src/main/java/io/producer/kafka/producers/WikimediaChangeProducer.java
@@ -0,0 +1,50 @@
+package io.producer.kafka.producers;
+
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import com.launchdarkly.eventsource.ConnectStrategy;
+import com.launchdarkly.eventsource.EventSource;
+import com.launchdarkly.eventsource.background.BackgroundEventHandler;
+import com.launchdarkly.eventsource.background.BackgroundEventSource;
+
+import io.producer.kafka.handlers.WikimediaEventHandler;
+
+public class WikimediaChangeProducer {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		Properties props = new Properties();
+
+		props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+		// properties for high throughput producer
+		props.setProperty(ProducerConfig.LINGER_MS_CONFIG, "5000");
+		props.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+		props.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024)); // 32kb
+
+		String topic = "wikimedia.recentchange";
+
+		KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(props);
+
+		BackgroundEventHandler eventHandler = new WikimediaEventHandler(kafkaProducer, topic);
+
+		String sourceUrl = "https://stream.wikimedia.org/v2/stream/recentchange";
+
+		BackgroundEventSource eventSource = new BackgroundEventSource.Builder(eventHandler,
+				new EventSource.Builder(ConnectStrategy.http(URI.create(sourceUrl)))).build();
+
+		eventSource.start();
+
+		TimeUnit.MILLISECONDS.sleep(5000);
+
+	}
+
+}
